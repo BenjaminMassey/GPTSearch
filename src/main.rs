e0d246afcddbcddbc164959fe9a82856a20b963c
Benjamin Massey <benjamin.w.massey@gmail.com>
2023-03-20 00:38:22 -0700

    Getting the links from the google search, but still need further parsing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate reqwest;
+extern crate regex;
 use chatgpt::prelude::*;
 use std::env;
 use chatgpt::types::CompletionResponse;
 use std::io::Read;
 use std::collections::HashMap;
 use std::error::Error;
+use scraper::{Html, Selector};
+use regex::Regex;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -17,12 +20,43 @@ async fn main() -> Result<()> {
 
     let search_url = base + search;
 
-    let resp = reqwest::get(search_url)
+    let response = reqwest::get(search_url)
         .await?
         .text()
         .await?;
 
-    println!("{:#?}", resp);
+    let document = scraper::Html::parse_document(&response);
+
+   let title_selector = scraper::Selector::parse("a").unwrap();
+
+    let titles = document.select(&title_selector).map(|x| x.html());
+
+    let mut results = Vec::new();
+
+    titles
+        .zip(1..101)
+        .for_each(|(item, number)| results.push(item));
+
+    // <a\s[^>]*href=["']([^"']+)["'][^>]*>
+    let qu = '"'.to_string();
+    let mut raw_r = r"<a\s[^>]*href=[".to_string();
+    raw_r.push_str(&qu);
+    raw_r.push_str(&(r"']([^".to_string()));
+    raw_r.push_str(&qu);
+    raw_r.push_str(&(r"']+)[".to_string())); 
+    raw_r.push_str(&qu);
+    raw_r.push_str(&(r"'][^>]*>".to_string())); 
+    let r = &raw_r[..];
+    
+    let re = Regex::new(r).unwrap();
+
+    for result in results.iter() {
+        match re.captures(result) {
+            Some(caps) => println!("Found: {}", caps.get(1).unwrap().as_str()),
+            None => println!("Found nothing...")
+        }
+    }
+    
 
     /*
     let args: Vec<String> = env::args().collect();
