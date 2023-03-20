aeecbfbe14e1eb4620ead7e3f88748a448ca9334
Benjamin Massey <benjamin.w.massey@gmail.com>
2023-03-19 22:20:07 -0700

    Basic google search http get

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
+extern crate reqwest;
 use chatgpt::prelude::*;
 use std::env;
 use chatgpt::types::CompletionResponse;
+use std::io::Read;
+use std::collections::HashMap;
+use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+
+    let args: Vec<String> = env::args().collect();
+
+    let search = &args[1] as &str;
+
+    let base = "https://www.google.com/search?q=".to_owned();
+
+    let search_url = base + search;
+
+    let resp = reqwest::get(search_url)
+        .await?
+        .text()
+        .await?;
+
+    println!("{:#?}", resp);
+
+    /*
     let args: Vec<String> = env::args().collect();
 
     let key = &args[1];
@@ -15,6 +36,6 @@ async fn main() -> Result<()> {
         .await?;
 
     println!("Response: {}", response.message().content);
-
+    */
     Ok(())
 }
